0cb442654e92bfc5f3a37606f1c7e3d793169e3b
Christian Carollo <carollochristian6@gmail.com>
2023-02-16 17:15:59 +0100

    Add business logic in MedicoService, Add CRUD in MedicoController

--- src/main/java/co/develhope/team1studiomedico/controllers/MedicoController.java
+++ src/main/java/co/develhope/team1studiomedico/controllers/MedicoController.java
@@ -1,9 +1,14 @@
 package co.develhope.team1studiomedico.controllers;
 
+import co.develhope.team1studiomedico.entities.Medico;
+import co.develhope.team1studiomedico.entities.Paziente;
 import co.develhope.team1studiomedico.services.MedicoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medico")
@@ -11,5 +16,44 @@ public class MedicoController {
 
     @Autowired
     private MedicoService medicoService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createMedico(@RequestBody Medico medico){
+        if(medico == null) throw new IllegalArgumentException();
+        medicoService.createMedico(medico);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Medico creato correttamente");
+    }
+
+    @GetMapping({"", "/"})
+    public List<Medico> getMedici(){
+        return medicoService.getMedici();
+    }
+
+    @GetMapping("/{id}")
+    public Medico getMedicoById(@PathVariable Long id) {
+        if(id == null) throw new IllegalArgumentException();
+        return medicoService.getMedicoById(id);
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<String> updateMedicoById(@RequestBody Medico medicoEdit, @PathVariable Long id) {
+        if(medicoEdit == null || id == null) throw new IllegalArgumentException();
+        medicoService.updateMedicoById(medicoEdit, id);
+        return ResponseEntity.status(200).body("Medico modificato correttamente");
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteMedici() {
+        medicoService.deleteMedici();
+        return ResponseEntity.status(200).body("Medici cancellati correttamente");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteMedicoById(@PathVariable Long id) {
+        if(id == null) throw new IllegalArgumentException();
+        medicoService.deleteMedicoById(id);
+        return ResponseEntity.status(200).body("Medico cancellato correttamente");
+    }
 
 }
--- src/main/java/co/develhope/team1studiomedico/services/MedicoService.java
+++ src/main/java/co/develhope/team1studiomedico/services/MedicoService.java
@@ -1,13 +1,59 @@
 package co.develhope.team1studiomedico.services;
 
+import co.develhope.team1studiomedico.entities.Medico;
+import co.develhope.team1studiomedico.entities.Paziente;
+import co.develhope.team1studiomedico.exceptions.NotFoundException;
 import co.develhope.team1studiomedico.repositories.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MedicoService {
 
     @Autowired
     private MedicoRepository medicoRepository;
+
+
+    public void createMedico(Medico medico) {
+        medico.setId(null);
+        medicoRepository.saveAndFlush(medico);
+    }
+
+    public List<Medico> getMedici() {
+        return medicoRepository.findAll();
+    }
+
+
+    public Medico getMedicoById(Long id) {
+        if(!medicoRepository.existsById(id)) throw new NotFoundException();
+        return medicoRepository.findById(id).get();
+    }
+
+    public void updateMedicoById(Medico medicoEdit, Long id) {
+        if(medicoEdit == null) throw new IllegalArgumentException();
+        if(!medicoRepository.existsById(id)) throw new NotFoundException();
+
+        Medico medico = medicoRepository.findById(id).get();
+
+        if(medicoEdit.getNome() != null) medico.setNome(medicoEdit.getNome());
+        if(medicoEdit.getCognome() != null) medico.setCognome(medicoEdit.getCognome());
+        if(medicoEdit.getTelefono() != null) medico.setTelefono(medicoEdit.getTelefono());
+        if(medicoEdit.getEmail() != null) medico.setEmail(medicoEdit.getEmail());
+
+        medicoRepository.saveAndFlush(medico);
+    }
+
+    public void deleteMedicoById(Long id) {
+        if(!medicoRepository.existsById(id)) throw new NotFoundException();
+        medicoRepository.deleteById(id);
+    }
+
+    public void deleteMedici() {
+        medicoRepository.deleteAll();
+    }
+
+
 
 }
