78fc57dfacb844048085e9e704995f725bdcf8cc
Dino <dino15031992@gmail.com>
2023-02-16 19:40:14 +0100

    add logic and CRUD SegretarioService and SegretarioController

--- src/main/java/co/develhope/team1studiomedico/controllers/SegretarioController.java
+++ src/main/java/co/develhope/team1studiomedico/controllers/SegretarioController.java
@@ -1,9 +1,13 @@
 package co.develhope.team1studiomedico.controllers;
 
+import co.develhope.team1studiomedico.entities.Segretario;
 import co.develhope.team1studiomedico.services.SegretarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/segretario")
@@ -11,5 +15,48 @@ public class SegretarioController {
 
     @Autowired
     private SegretarioService segretarioService;
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createSegretario(@RequestBody Segretario segretario){
+        if(segretario == null) throw new IllegalArgumentException();
+        segretarioService.createSegretario(segretario);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Segretario creato correttamente");
+    }
+
+    @GetMapping("/{id}")
+    public Segretario getSegretarioById(@PathVariable Long id) {
+        if(id == null) throw new IllegalArgumentException();
+        return segretarioService.getSegretarioById(id);
+    }
+
+    @GetMapping({"", "/"})
+    public List<Segretario> getSegretari(){
+        return segretarioService.getSegretari();
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<String> updateSegretarioById(@RequestBody Segretario segretarioEdit, @PathVariable Long id) {
+        if(segretarioEdit == null || id == null) throw new IllegalArgumentException();
+        segretarioService.updateSegretarioById(segretarioEdit, id);
+        return ResponseEntity.status(200).body("Segretario modificato correttamente");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteSegreatrioById(@PathVariable Long id) {
+
+        if(id == null) throw new IllegalArgumentException();
+        segretarioService.deleteSegretarioById(id);
+        return ResponseEntity.status(200).body("Segretario cancellato correttamente");
+
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteSegretari() {
+        segretarioService.deleteSegretari();
+        return ResponseEntity.status(200).body("Segretari cancellati correttamente");
+    }
+
+
+
 
 }
--- src/main/java/co/develhope/team1studiomedico/services/SegretarioService.java
+++ src/main/java/co/develhope/team1studiomedico/services/SegretarioService.java
@@ -1,13 +1,67 @@
 package co.develhope.team1studiomedico.services;
 
+import co.develhope.team1studiomedico.entities.Segretario;
+import co.develhope.team1studiomedico.exceptions.NotFoundException;
 import co.develhope.team1studiomedico.repositories.SegretarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class SegretarioService {
 
     @Autowired
     private SegretarioRepository segretarioRepository;
+
+    public void createSegretario(Segretario segretario) {
+
+        segretario.setId(null);
+        segretarioRepository.saveAndFlush(segretario);
+
+    }
+
+    public Segretario getSegretarioById(Long id) {
+
+        if (!segretarioRepository.existsById(id)) throw new NotFoundException();
+        return segretarioRepository.findById(id).get();
+
+    }
+
+    public List<Segretario> getSegretari() {
+
+        return segretarioRepository.findAll();
+
+    }
+
+    public void updateSegretarioById(Segretario segretarioEdit, Long id) {
+
+        if (segretarioEdit == null) throw new IllegalArgumentException();
+        if (!segretarioRepository.existsById(id)) throw new NotFoundException();
+
+        Segretario segretario = segretarioRepository.findById(id).get();
+
+        if (segretarioEdit.getNome() != null) segretario.setNome(segretarioEdit.getNome());
+        if (segretarioEdit.getCognome() != null) segretario.setCognome(segretarioEdit.getCognome());
+        if (segretarioEdit.getTelefono() != null) segretario.setTelefono(segretarioEdit.getTelefono());
+        if (segretarioEdit.getEmail() != null) segretario.setEmail(segretarioEdit.getEmail());
+
+        segretarioRepository.saveAndFlush(segretario);
+
+    }
+
+    public void deleteSegretarioById(Long id) {
+
+        if(!segretarioRepository.existsById(id)) throw new NotFoundException();
+        segretarioRepository.deleteById(id);
+
+    }
+
+    public void deleteSegretari() {
+
+        segretarioRepository.deleteAll();
+
+    }
+
+
 
 }
